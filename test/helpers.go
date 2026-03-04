6c910704870674dc62425b65ffee780af3f57a19
Maelkum <aleksandar.cekrlic@gmail.com>
2026-03-04 15:22:13 +0100

    Add configurable test DB

--- test/helpers.go
+++ test/helpers.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"cmp"
 	"context"
+	"os"
 	"testing"
 	"time"
 
@@ -11,12 +13,15 @@ import (
 	database "github.com/xmtp/example-notification-server-go/pkg/db"
 )
 
-const TEST_DSN = "postgres://postgres:xmtp@localhost:25432/postgres?sslmode=disable"
+var testDSN = cmp.Or(
+	os.Getenv("XMTP_NOTIFICATION_SERVER_TEST_DSN"),
+	"postgres://postgres:xmtp@localhost:25432/postgres?sslmode=disable",
+)
 
 func createDb(t *testing.T) *bun.DB {
 	t.Helper()
 
-	db, err := database.CreateBunDB(TEST_DSN, 5*time.Second)
+	db, err := database.CreateBunDB(testDSN, 5*time.Second)
 	require.NoError(t, err)
 
 	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
